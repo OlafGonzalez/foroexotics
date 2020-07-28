b26f9a2d416175c5186abfff10fff3f925ea68d8
Olaf Gonzalez Cortes <olaf.gonzalez1309@gmail.com>
2020-07-27 23:11:09 -0500

    View emulator data in card
    Not install moto z

---
+++ lib/Carpost.dart
@@ -0,0 +1,25 @@
+class CarPost{
+  String 
+  modelo,
+  marca,
+  imagen,
+  autoproducidos,
+  numeroAuto,
+  precioSalida,
+  precioActual,
+  year,
+  time,
+  date;
+  CarPost(
+  this.modelo,
+  this.marca,
+  this.imagen,
+  this.autoproducidos,
+  this.numeroAuto,
+  this.precioSalida,
+  this.precioActual,
+  this.year,
+  this.time,
+  this.date);
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:ui';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'agregarCarro.dart';
+import 'Carpost.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +20,38 @@ class _MyAppState extends State<MyApp> {
   FirebaseAuth _auth = FirebaseAuth.instance;
   FirebaseUser _user;
   GoogleSignIn _googleSignIn = new GoogleSignIn();
+  List<CarPost> carpostList = [];
+  @override
+  void initState() {
+    super.initState();
+    DatabaseReference postReference = FirebaseDatabase.instance.reference().child("Post");
+    postReference.once().then((DataSnapshot snap){
+      var keys = snap.value.keys;
+      var data = snap.value;
+
+      carpostList.clear();
+      
+      for(var individualKey in keys){
+        CarPost posts = CarPost(
+          data[individualKey]['modelo'],
+          data[individualKey]['marca'],
+          data[individualKey]['image'],
+          data[individualKey]['autoP'],
+          data[individualKey]['numeroA'],
+          data[individualKey]['psalida'],
+          data[individualKey]['pactual'],
+          data[individualKey]['year'],
+          data[individualKey]['time'],
+          data[individualKey]['date'],
+        );
+        carpostList.add(posts);
+      }
+      setState(() {
+        print('Length: $carpostList.length');
+      });
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +64,22 @@ class _MyAppState extends State<MyApp> {
           title: Text("Foro"),
         ),
         body: Center(
-          child: ListView(
-            children: <Widget>[
-              
-            ],
-          ),
+          child: carpostList.length == 0
+          ? Center(
+            child: Text("No existen post"),
+          )
+          : Center(
+            child: ListView.builder(
+              itemCount: carpostList.length,
+              itemBuilder: (_,index){
+                return postUI(
+                  carpostList[index].marca,
+                  carpostList[index].modelo,
+                  carpostList[index].imagen
+                );
+              },
+            ),
+          )
         ),
         floatingActionButton: StreamBuilder<Object>(
             stream: null,
@@ -47,6 +94,29 @@ class _MyAppState extends State<MyApp> {
                 child: Icon(Icons.add),
               );
             }),
+      ),
+    );
+  }
+
+  Widget postUI(String marca,String modelo, String image){
+    return Card(
+      elevation: 10.0,
+      margin: EdgeInsets.all(14),
+      child: Container(
+        padding: EdgeInsets.all(14),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment:  MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(marca),
+                Text(modelo),
+                Image.network(image,fit: BoxFit.cover,)
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
