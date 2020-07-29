9a43ecead5a52d5bf3cfe25263f8f515adfd9c3d
Olaf Gonzalez Cortes <olaf.gonzalez1309@gmail.com>
2020-07-28 22:01:02 -0500

    css coments

--- lib/carinfo.dart
+++ lib/carinfo.dart
@@ -88,27 +88,27 @@ class _CarinfoState extends State<Carinfo> {
   @override
   void initState() {
     super.initState();
-    DatabaseReference postReference =
-        FirebaseDatabase.instance.reference().child("/comentarios/"+keyPostRecibe+"");
-        postReference.once().then((DataSnapshot snap) {
-          var keys = snap.value.keys;
-          var data = snap.value;
-          comentariosList.clear();
+    DatabaseReference postReference = FirebaseDatabase.instance
+        .reference()
+        .child("/comentarios/" + keyPostRecibe + "");
+    postReference.once().then((DataSnapshot snap) {
+      var keys = snap.value.keys;
+      var data = snap.value;
+      comentariosList.clear();
 
-          for(var comentsKeys in keys){
-            Comentarios comen = Comentarios(
-              data[comentsKeys]['date'],
-              data[comentsKeys]['time'],
-              data[comentsKeys]['comentario'],
-              data[comentsKeys]['userNombre'],
-              data[comentsKeys]['userImagen']
-            );
-            comentariosList.add(comen);
-          }
-          setState(() {
-            print('Length: $comentariosList.length');
-          });
-        });
+      for (var comentsKeys in keys) {
+        Comentarios comen = Comentarios(
+            data[comentsKeys]['date'],
+            data[comentsKeys]['time'],
+            data[comentsKeys]['comentario'],
+            data[comentsKeys]['userNombre'],
+            data[comentsKeys]['userImagen']);
+        comentariosList.add(comen);
+      }
+      setState(() {
+        print('Length: $comentariosList.length');
+      });
+    });
   }
 
   @override
@@ -119,20 +119,20 @@ class _CarinfoState extends State<Carinfo> {
         ),
         home: Scaffold(
             backgroundColor: Colors.black,
-            appBar: AppBar(  
+            appBar: AppBar(
               actions: <Widget>[
                 Container(
-                    width: 180,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage: NetworkImage(imageuserRecibe),
-                        ),
-                        Text(usernameRecibe),
-                      ],
-                    ),
-                  )
+                  width: 180,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage: NetworkImage(imageuserRecibe),
+                      ),
+                      Text(usernameRecibe),
+                    ],
+                  ),
+                )
               ],
               title: Text("Info"),
             ),
@@ -152,112 +152,163 @@ class _CarinfoState extends State<Carinfo> {
                   ),
                 ),
                 SliverList(
-                  delegate: SliverChildListDelegate(
-                    [
-                      Container(
-                        height: 200,
-                        width: 60,
-                        padding: EdgeInsets.all(10),
-                        //color: Colors.yellow,
-                        child: Form(
-                          key: formkey,
-                          child: Card(
-                            elevation: 10.0,
-                            child: Column(
-                              children: <Widget>[
-                                Container(
+                  delegate: SliverChildListDelegate([
+                    Container(
+                      height: 170,
+                      width: 60,
+                      padding: EdgeInsets.all(1),
+                      //color: Colors.yellow,
+                      child: Form(
+                        key: formkey,
+                        child: Card(
+                          elevation: 10.0,
+                          child: Column(
+                            children: <Widget>[
+                              Container(
                                   //TextFormfield comentario
                                   padding: EdgeInsets.all(10),
                                   child: TextFormField(
                                     onSaved: (value) => _comentario = value,
-                                    validator:(value){
-                                        if(value.isEmpty){
-                                          return "Ingrese un comentario antes!!";
-                                        }
-                                      },
+                                    validator: (value) {
+                                      if (value.isEmpty) {
+                                        return "Ingrese un comentario antes!!";
+                                      }
+                                    },
                                     decoration: InputDecoration(
-                                      hintText: "Comentario",
-                                      prefixIcon: Icon(Icons.comment),
-                                      border: OutlineInputBorder(
-                                        borderRadius: BorderRadius.circular(15)
-                                      )
-                                    ),
-                                  )
-                                ),
-                                Container(
-                                  child: RaisedButton(
+                                        hintText: "Comentario",
+                                        prefixIcon: Icon(Icons.comment),
+                                        border: OutlineInputBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(15))),
+                                  )),
+                              Container(
+                                child: RaisedButton(
                                     child: Text("Publicar"),
-                                    onPressed: (){
-                                      ComentarioToFireBase(keyPostRecibe);   
-                                    }
-                                    ),
-                                )
-                              ],
-                            ),
+                                    onPressed: () {
+                                      ComentarioToFireBase(keyPostRecibe);
+                                    }),
+                              )
+                            ],
                           ),
                         ),
                       ),
-                      
-                    ]
-                  ),      
-                  ),
-                  SliverList(delegate: SliverChildListDelegate(
-                    List.generate(comentariosList.length, (index){
-                      return cardComen(
-                        comentariosList[index].comentario, 
-                        comentariosList[index].date, 
-                        comentariosList[index].time,
-                        comentariosList[index].userName,
-                        comentariosList[index].userImage);
-                    })))
+                    ),
+                  ]),
+                ),
+                SliverList(
+                    delegate: SliverChildListDelegate(
+                        List.generate(comentariosList.length, (index) {
+                  return cardComen(
+                      comentariosList[index].comentario,
+                      comentariosList[index].date,
+                      comentariosList[index].time,
+                      comentariosList[index].userName,
+                      comentariosList[index].userImage);
+                })))
               ],
-            )
-            )
-            );
+            )));
   }
 
-Widget cardComen(String comentario,String date,String time,String userName,String userImage){
-  return Card(
-    elevation: 14.0,
-    color: Colors.white,
-    child: Container(
-      padding: EdgeInsets.all(10),
-      child: Row(
-        children: <Widget>[
-          
-        ],
-      ),
-    ),
-  );
-}
+  Widget cardComen(String comentario, String date, String time, String userName,
+      String userImage) {
+    return Card(
+      elevation: 14.0,
+      color: Colors.white,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                CircleAvatar(
+                  maxRadius: 18,
+                  backgroundImage: NetworkImage(imageuserRecibe),
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                //UserName
+                Container(
+                  width:290 ,
+                  child: 
+                  Text(
+                    userName,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold
+                    ),),
+                ),
+                //Comentario
+                Container(
+                  width: 290,
+                  child: Text(comentario,
+                  style: TextStyle(
 
-bool validateandSave(){
+                    fontSize: 16
+                  ),
+                  ),
+                ),
+                Container(
+                  height: 10,
+                ),
+
+                Container(
+                  width: 290,
+                  child: Text(date +" "+ time,
+                  style: TextStyle(
+                    fontSize: 11,
+                    fontWeight: FontWeight.bold
+                  ),
+                  ),
+                ),
+                Container(
+                  width: 290,
+                  child: Text("Sign in whit Google",
+                  style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.bold
+                  ),),
+                  
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  bool validateandSave() {
     final form = formkey.currentState;
-    if(form.validate()){
+    if (form.validate()) {
       form.save();
       return true;
-    }else{
+    } else {
       return false;
     }
   }
 
-
-  void ComentarioToFireBase(String keypost){
-    if(validateandSave()){
-    var dbTimerKey = DateTime.now();
-    var formatDate = DateFormat('MMM d, yyyy');
-    var formatTime = DateFormat('EEEE, hh:mm aaa');
-    String date = formatDate.format(dbTimerKey);
-    String time = formatTime.format(dbTimerKey);
-    DatabaseReference ref = FirebaseDatabase.instance.reference();
-    var data ={
-      "date":date,
-      "time":time,
-      "comentario":_comentario,
-      "userNombre":usernameRecibe,
-      "userImagen":imageuserRecibe
-    };
-    ref.child("/comentarios/"+keypost+"").push().set(data);
+  void ComentarioToFireBase(String keypost) {
+    if (validateandSave()) {
+      var dbTimerKey = DateTime.now();
+      var formatDate = DateFormat('MMM d, yyyy');
+      var formatTime = DateFormat('EEEE, hh:mm aaa');
+      String date = formatDate.format(dbTimerKey);
+      String time = formatTime.format(dbTimerKey);
+      DatabaseReference ref = FirebaseDatabase.instance.reference();
+      var data = {
+        "date": date,
+        "time": time,
+        "comentario": _comentario,
+        "userNombre": usernameRecibe,
+        "userImagen": imageuserRecibe
+      };
+      ref.child("/comentarios/" + keypost + "").push().set(data);
     }
   }
 }
